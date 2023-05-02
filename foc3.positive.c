f79b4a7c456e5f8c7432ed8306254f47ff22ac00
BhanuVard <128610358+BhanuVard@users.noreply.github.com>
2023-05-02 14:42:29 +0530

    positive or negative

---
+++ foc3.positive.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+   printf("Enter an integer: ");
+    scanf("%d",&n);
+    if(n>0)
+    printf("positive:%d",n);
+    else
+  printf("negative:%d",n);
+
+    return 0;
+}
+
