1ad367c9d580bbcddcf9d36d73f5cfa8e679386b
BhanuVard <128610358+BhanuVard@users.noreply.github.com>
2023-05-09 13:15:30 +0530

    output

---
+++ test 7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    sum = n * n;
+
+    printf("The sum of the odd number series is: %d", sum);
+
+    return 0;
+}
