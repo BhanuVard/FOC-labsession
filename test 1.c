b75ba17568c34cbf93d43978507daf9aa4ced7a9
BhanuVard <128610358+BhanuVard@users.noreply.github.com>
2023-05-14 22:43:22 +0530

    n numbers

---
+++ test 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("The number series from 1 to %d is:\n", n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
