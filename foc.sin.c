ada232f545cb375b8d866a0d99d1b51f8f68d1da
BhanuVard <128610358+BhanuVard@users.noreply.github.com>
2023-05-17 10:07:38 +0530

    sin series

---
+++ foc.sin.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int fact(int x){
+	int i,fact=1;
+	for(i=1;i<=x;i++){
+		fact=fact*i;
+	}
+return fact;
+}
+double convert(double degree)
+{
+	double pi=3.14;
+	return(degree*(pi/180));}
+	void main(){
+		int x,y,i,j,n;
+		float sum=0.0;
+		printf("enter x");
+		scanf("%d",&x);
+		printf("enter n");
+		scanf("%d",&n);
+		y=x;
+		double r=convert(x)
+		for(i=1;j=1;i<=n;i++;j=j+2)
+		{
+			if(i%2==0)
+			{
+				sum=sum-power(r,j)/fact(j);
+			}
+			else{
+			sum=sum+power(r,j)/fact(j);	
+			}
+		}
+		printf("sin(%d)=%f",y,sum);
+	}
+
